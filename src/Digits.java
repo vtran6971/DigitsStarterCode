24352e6c196b00f11b2ee20f4f8ebfe9a1e26c65
BT_1N3_15 <wchen1325@bths.edu>
2019-04-05 08:35:18 -0400

    digits working

--- src/Digits.java
+++ src/Digits.java
@@ -4,17 +4,30 @@ import java.lang.*;
 public class Digits
 {
     //The list of digits from the number used to //construct this object
-    ArrayList<Integer> digitList;
+    ArrayList<Integer> digitList = new ArrayList<>();
 
     //Constructs a Digits object that
     //represents num
     public Digits (int num)
     {
         //implemented in part (a)
+        for (int i = num; i > 0; i = i%10) {
+            digitList.add(i);
+            i = i/10;
+        }
     }
     public boolean isStrictlyIncreasing()
     {
+        boolean bool = true;
         //implemented in part (b)
+        for(int j = 0; j < digitList.size()-1; j++)
+        {
+            if(digitList.get(j) > digitList.get(j+1))
+            {
+                bool = false;
+            }
+        }
+        return bool;
     }
     //Extra method to make the runner easier to read
     public ArrayList<Integer> getDigitList()
